900c6f4dea04651df946e4bc8857825defe41a58
Noah Carpenter <noahcarpenter@MacbookAir-33928.local>
2018-04-24 15:13:03 -0700

    add javascript function, first attempt at an event listener(needs work)

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,25 @@
-var locations = [
-  'Location 1',
-  'Location 2',
-  'Location 3',
-  'Location 4',
-  'Location 5',
-  'Location 6',
-]
-var random= Math.floor((Math.random() * locations.length));
-var randomLocation=locations[random];
-console.log(randomLocation);
+
+function getRandomLocation() {
+  var locations = [
+    'Junk Junction',
+    'Haunted Hills',
+    'Snobby Shores',
+    'Greasy Grove',
+    'Shifty Shafts',
+    'Flush Factory',
+    'Fatal Fields',
+    'Tilted Towers',
+    'Pleasant Park',
+    'Loot Lake',
+    'Anarchy Acres',
+    'Tomato Town',
+    'Wailing Woods',
+    'Lonely Lodge',
+    'Moisty Mire',
+    'Dusty Depot',
+    'Lucky Landing',
+  ]
+  var random=Math.floor((Math.random() * locations.length));
+  var randomLocation=locations[random];
+  console.log(randomLocation);
+}
